246c72a67402fdd1f6d70b990f7f9e0e38c8b9c6
jordana-rosa <jordananabarbosa@hotmail.com>
2024-08-08 21:59:01 -0300

    algumas outras tarefas

---
+++ Jordana Rosa/Singleton/app-config.js
@@ -0,0 +1,32 @@
+class AppConfig {
+  constructor() {
+      if (AppConfig.instance) {
+          return AppConfig.instance;
+      }
+
+      this.apiKey = 'JLSDJIDH123898SDHKHD';
+      this.apiUrl = 'http://mock.com.br';
+      AppConfig.instance = this;
+  }
+
+  static getInstance() {
+      if (!AppConfig.instance) {
+          AppConfig.instance = new AppConfig();
+      }
+      return AppConfig.instance;
+  }
+
+  setApiUrl(apiUrl) {
+      this.apiUrl = apiUrl;
+  }
+
+  // Método para fazer uma solicitação à API usando a configuração
+  fetchData() {
+      if (!this.apiUrl) {
+          throw new Error('Url da API não pode ser nula');
+      }
+      console.log(`Realizando uma chamada para a URL: ${this.apiUrl}`);
+  }
+}
+
+export default AppConfig;
